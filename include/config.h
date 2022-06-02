1b5ac1160b31a9fd1d3cfadbac23c721bcd9cd01
陈二爷 <36294803+z52c@users.noreply.github.com>
2022-06-02 15:46:13 +0800

    Create config.h

---
+++ include/config.h
@@ -0,0 +1,14 @@
+#define CONFIG_H
+
+#define TDXDIR "C:\\file\\program\\new_tdx\\"
+#define TDXSHMIN1DIR "C:\\file\\program\\new_tdx\\vipdoc\\sh\\minline\\"
+#define TDXSHDAYDIR "C:\\file\\program\\new_tdx\\vipdoc\\sh\\lday\\"
+#define TDXSHMIN5DIR "C:\\file\\program\\new_tdx\\vipdoc\\sh\\fzline\\"
+#define TDXSZMIN1DIR "C:\\file\\program\\new_tdx\\vipdoc\\sz\\minline\\"
+#define TDXSZDAYDIR "C:\\file\\program\\new_tdx\\vipdoc\\sz\\lday\\"
+#define TDXSZMIN5DIR "C:\\file\\program\\new_tdx\\vipdoc\\sz\\fzline\\"
+
+#define FHPGDBFILE "./fhpg/fhpg.db"  //分红配股数据库文件位置
+#define STOCKLISTFILE "stockList.csv"   //股票列表文件
+
+#endif
